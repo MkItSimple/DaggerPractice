9aad763ddf7c18265ba6f48acd43d7da771f6475
MkItSimple <choanania1@gmail.com>
2020-02-11 15:33:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/daggerpractice/di/ActivityBuildersModule.java
@@ -0,0 +1,16 @@
+package com.example.daggerpractice.di;
+
+import com.example.daggerpractice.AuthActivity;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+
+}
